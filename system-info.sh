72c61e70f8ed0e611c528c3ad66fc58e65ce163c
root <root@server.(none)>
2015-10-24 09:49:47 -0600

    initial commit

---
+++ system-info.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Author: Author of this clumsy bash script is Mr.Prashanth Goriparthi
+
+spec_time=`date +%s`
+host=`hostname`
+
+mem=`free -m | gawk  '/Mem:/{print $2}'`
+proc_count=`cat /proc/cpuinfo | grep processor | wc -l`
+os=`cat /etc/redhat-release | grep -o '[0-9].[0-9].[0-9]*'`
+uptime=$(uptime | grep -o "[0-9]* days" | awk  ' { print $1 } ')
+
+echo $host.system-info.operating-system ${os} $spec_time
+echo $host.system-info.total-memory ${mem} $spec_time
+echo $host.system-info.total-processor-count ${proc_count} $spec_time
+echo $host.system-info.uptime ${uptime} $spec_time
